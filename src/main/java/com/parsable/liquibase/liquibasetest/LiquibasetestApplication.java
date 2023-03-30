e8d46192152370c78f8f759bc78519c8085d39b4
Dmitry Karachentsev <dmitry.karachentsev@parsable.com>
2023-03-30 15:13:50 +0300

    init

---
+++ src/main/java/com/parsable/liquibase/liquibasetest/LiquibasetestApplication.java
@@ -0,0 +1,13 @@
+package com.parsable.liquibase.liquibasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibasetestApplication.class, args);
+	}
+
+}
